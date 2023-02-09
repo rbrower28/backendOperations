0617c69db7f20bc59c0e4c30f79071ea5ba614d7
Ryan <ryan.brower28@gmail.com>
2023-02-09 14:40:47 -0700

    secure data at PUT

---
+++ README.md
@@ -0,0 +1,10 @@
+# Backend Web Development Application
+### Ryan Brower
+
+## Features include:
+
+* Basic CRUD operations
+* Data Validation:
+* Error Handling:
+* On PUT requests, data not specified in the request will remain unchanged.
+* A timestamp is added to each POST request that cannot be modified with PUT.
--- controllers/client.js
+++ controllers/client.js
@@ -38,17 +38,30 @@ const createClient = async (req, res) => {
 
 const updateClient = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const client = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email
-  };
-  const response = await mongodb.getDb().db('tmp').collection('client').replaceOne({ _id: userId }, client);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res.status(500).json(response.error || 'Something happened in contact.');
+
+  // gather previous data
+  const result = await mongodb.getDb().db('tmp').collection('client').find({ _id: userId });
+  result.toArray().then((lists) => {
+    updateData(lists[0]);
+  });
+
+  async function updateData(item) {
+    
+    // data not included in put request will remain
+    const client = {
+      firstName: req.body.firstName || item.firstName,
+      lastName: req.body.lastName || item.lastName,
+      email: req.body.email || item.email,
+      time_created: item.time_created
+    };
+
+    const response = await mongodb.getDb().db('tmp').collection('client').replaceOne({ _id: userId }, client);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Something happened in contact.');
+    }
   }
 };
 
