6731da5080183b5f0dbc0dd192a389e37334d09c
Ryan <ryan.brower28@gmail.com>
2023-02-21 15:18:16 -0700

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
 const mongodb = require("./db/connect");
+const cors = require("cors");
 
 // can switch between localhost or current deployed port
 const port = process.env.port || 8080
